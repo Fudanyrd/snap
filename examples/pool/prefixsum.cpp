1763c22a6d0557ee3f9dc262578a52089a3d231c
Rundong Yang <143480204+Fudanyrd@users.noreply.github.com>
2025-10-06 06:56:59 +0000

    [BUG] fix examples of thread pool

--- examples/pool/prefixsum.cpp
+++ examples/pool/prefixsum.cpp
@@ -1,8 +1,10 @@
 #include "thread.h"
 #include <assert.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#define N 8192
+#define N (TPOOL_WORKERS * 1024)
 
 static int A[N];
 static int B[N];
@@ -45,6 +47,7 @@ static void add_task(void **args) {
 }
 
 int main(int argc, char **argv) {
+  ThreadPool pool;
   /* Initialize buffer */
   const int seglen = N / TPOOL_WORKERS;
   for (int i = 0; i < TPOOL_WORKERS; i++) {
@@ -53,40 +56,46 @@ int main(int argc, char **argv) {
     t.args[0] = (void *)&(A[i * seglen]);
     t.args[1] = (void *) 1;
     t.args[2] = (void *)seglen;
+    pool.push(&t);
   }
-  tpool.AddTasks(taskBuf, TPOOL_WORKERS);
+  pool.waitForAll();
+  for (int i = 0; i < N; i++) {
+    if (A[i] != 1) {
+      fprintf(stderr, "At i = %d\n", i);
+      abort();
+    }
+  }
+  
   for (int i = 0; i < TPOOL_WORKERS; i++) {
     Task &t = taskBuf[i];
-    t.waitfor();
     t.routine = prefixsum_task;
     t.args[0] = (void *)&(A[i * seglen]);
     t.args[1] = (void *)&(B[i * seglen]);
     t.args[2] = (void *)seglen;
+    pool.push(&t);
   }
-  tpool.AddTasks(taskBuf, TPOOL_WORKERS);
+  pool.waitForAll();
 
   int aux[TPOOL_WORKERS];
-  aux[0] = 0;
-  taskBuf[0].waitfor();
+  aux[0] = B[seglen - 1];
   for (int i = 1; i < TPOOL_WORKERS; i++) {
     Task &t = taskBuf[i];
-    t.waitfor();
     const int last = aux[i - 1];
     aux[i] = B[(i + 1) * seglen - 1] + last;
     t.routine = add_task;
     t.args[0] = (void *)&(B[i * seglen]);
     t.args[1] = (void *) last;
     t.args[2] = (void *)seglen;
+    pool.push(&t);
   }
-  tpool.AddTasks(&taskBuf[1], TPOOL_WORKERS - 1);
-  for (int i = 1; i < TPOOL_WORKERS; i++) {
-    Task &t = taskBuf[i];
-    t.waitfor();
-  }
+  pool.waitForAll();
 
   for (int i = 0; i < N; i++) {
-    assert(A[i] == 1);
-    assert(B[i] == i + 1);
+    // assert(B[i] == i + 1);
+    if (B[i] != i + 1) {
+      fprintf(stderr, "At i = %d, B[i] = %d\n", i, B[i]);
+      abort();
+    }
   }
 
   return 0;
